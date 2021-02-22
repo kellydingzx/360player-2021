1e6fb97b6cdcb1594798c2aa1983b81493829bae
kellydingzx <55750344+kellydingzx@users.noreply.github.com>
2021-02-22 23:09:59 +0800

    2.22

--- Assets/Scripts/VideoManager.cs
+++ Assets/Scripts/VideoManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class VideoManager : MonoBehaviour
@@ -8,6 +9,20 @@ public class VideoManager : MonoBehaviour
     public VideoPlayer videoplayer;
     public GameObject playButton;
     public GameObject pauseButton;
+
+    public GameObject displayWindow;
+    public GameObject videoURLdisplay;
+
+    public GameObject changeVideoButton;
+    public GameObject backVideoButton;
+    
+
+    //Variables for switching videos
+    private GameObject[] objs;
+    private List<GameObject> needs_back;
+    private long location;
+    private string url_old_video;
+    private Vector3 camera_pos;
 
     void Start()
     {
@@ -28,5 +43,50 @@ public class VideoManager : MonoBehaviour
             pauseButton.SetActive(true);
             playButton.SetActive(false);
         }
+    }
+
+    public void ChangeVideo()
+    {
+        //Record the old video;
+        videoplayer.Stop();
+        location = videoplayer.frame;
+        url_old_video = videoplayer.url;
+        
+        //Play second video
+        PlaySecondVideo(videoURLdisplay.GetComponent<Text>().text);
+    }
+
+    public void PlaySecondVideo(string video_path)
+    {
+        videoplayer.url = video_path;
+        videoplayer.Play();
+        camera_pos = Camera.main.transform.position;
+        //Deactive all buttons from the previous video
+        objs = GameObject.FindGameObjectsWithTag("Trigger");
+        needs_back = new List<GameObject>();
+        foreach (GameObject button in objs)
+        {
+            if (button.activeSelf)
+            {
+                needs_back.Add(button);
+            }
+            button.SetActive(false);
+        }
+        displayWindow.SetActive(false);
+        backVideoButton.SetActive(true);
+    }
+
+    public void BacktoPrevVideo()
+    {
+        videoplayer.Stop();
+        videoplayer.url = url_old_video;
+        videoplayer.frame = location;
+        Camera.main.transform.rotation = Quaternion.Euler(camera_pos);
+        foreach (GameObject button in needs_back)
+        {
+            button.SetActive(true);
+        }
+        videoplayer.Play();
+        backVideoButton.SetActive(false);
     }
 }
